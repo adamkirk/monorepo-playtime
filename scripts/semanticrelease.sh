0637fe4b53b556d8dc8d3df7ae0f193d543f6887
Adam Kirk <adam_kirk@live.co.uk>
2026-05-14 19:54:54 +0100

    fix: missing module
    Fix error, touch all projects.

--- scripts/semanticrelease.sh
+++ scripts/semanticrelease.sh
@@ -3,8 +3,6 @@
 npx --yes \
     --package semantic-release@23 \
     --package conventional-changelog-conventionalcommits@8 \
-    --package @semantic-release/commit-analyzer@13 \
-    --package @semantic-release/release-notes-generator@14 \
     --package @semantic-release/github@10 \
     --package @semantic-release/exec@6 \
     semantic-release "$@"

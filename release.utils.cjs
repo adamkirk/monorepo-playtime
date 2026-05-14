1ecb91679c38f1e1f1725b1c2279392cc72dcad1
Adam Kirk <adam_kirk@live.co.uk>
2026-05-14 21:02:14 +0100

    fix(ci): add scopes to configs
    Adds the scope explicitly in release configs.

--- release.utils.cjs
+++ release.utils.cjs
@@ -3,10 +3,10 @@ const ALL_SCOPES = ['api', 'ui', 'cli', 'chart-api', 'chart-ui', 'ci'];
 function buildReleaseRules(scope) {
   return [
     ...ALL_SCOPES.filter(s => s !== scope).map(s => ({ scope: s, release: false })),
-    { type: 'feat', release: 'minor' },
-    { type: 'fix', release: 'patch' },
-    { type: 'perf', release: 'patch' },
-    { type: 'revert', release: 'patch' },
+    { type: 'feat', scope: scope, release: 'minor' },
+    { type: 'fix', scope: scope, release: 'patch' },
+    { type: 'perf', scope: scope, release: 'patch' },
+    { type: 'revert', scope: scope, release: 'patch' },
   ];
 }
 
